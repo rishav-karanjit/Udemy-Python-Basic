b8ecff0a46b6125c4f1a7aa4021f904cfac8401d
unknown <karanjitrishav4@gmail.com>
2020-10-22 22:24:03 +0545

    Added 5. user defined func

---
+++ 5. User defined function/1. Function intro.py
@@ -0,0 +1,11 @@
+# A function is a block of code which only runs when it is called.
+
+# def function_name():
+#     statement
+
+def PrintHello():
+    print("Hello")
+
+print("I am outside function")
+PrintHello()
+print("This is after func call")
---
+++ 5. User defined function/2. Passing argument to a function.py
@@ -0,0 +1,7 @@
+def PrintHello():
+    print("Hello",name1)
+    print("Hello",name2)
+
+name1 = "Rishav"
+name2 = "XYZ"
+PrintHello()
