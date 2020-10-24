486acc2d5d7ff704a5d20046802e1f27ba29056d
unknown <karanjitrishav4@gmail.com>
2020-10-24 17:02:16 +0530

    Added comments

--- 6. Project 1/1. Getting the data.py
+++ 6. Project 1/1. Getting the data.py
@@ -1,8 +1,12 @@
-a = int(input("Enter a integer:"))
+# input() -> Takes user input in the form of string
+
+a = int(input("Enter a integer:")) #We have changed string to integer because operations like addition cannot be performed in string
 b = int(input("Enter a integer:"))
 
-sign = input("Enter a sign: \n+ for addtion \n - for substraction \n * for multiplication \n / for division \n")
+sign = input("Enter \n + for addition \n - for substraction \n * for multiplication \n / for division\n")
+#No need to convert sign variable to integer because we don't expect integer in sign
 
 print("a =",a)
 print("b =",b)
+
 print("sign =",sign)
